c51c90a6685503b79835813a4dfe8c4ea5e85761
nathee56 <raphiphat6987@gmail.com>
2025-05-22 10:40:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// เสริมลูกเล่นเวลาเลื่อน
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
